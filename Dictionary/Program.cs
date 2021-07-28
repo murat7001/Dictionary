8a29ae1bad4e1f8e486bcf766bbfe1a2bad1d6a8
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2021-07-28 11:03:38 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> AdYas = new MyDictionary<string, int>();
+            AdYas.Add("Murat", 19);
+            AdYas.Add("Çağrı",19);
+
+            foreach (var item in AdYas.value)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
